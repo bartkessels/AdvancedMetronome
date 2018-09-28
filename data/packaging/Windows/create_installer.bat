82271d9478270647cea5597dfebc523599adfdac
Bart Kessels <bartkessels@bk-mail.com>
2018-09-29 01:19:43 +0200

    Fix incorrect license path

--- data/packaging/Windows/create_installer.bat
+++ data/packaging/Windows/create_installer.bat
@@ -1,6 +1,6 @@
 @echo off
 
-copy ..\..\..\COPYING meta\license.txt /y
+copy ..\..\..\COPYING application\meta\license.txt /y
 xcopy ..\..\..\bin application\data /h /e /i /y
 
 binarycreator -c config.xml -p . AdvancedMetronomeInstaller
